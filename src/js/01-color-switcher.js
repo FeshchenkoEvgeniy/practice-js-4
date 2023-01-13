c3d6375d05dd5dae2de67afd5c0c43be2d1cc5a3
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-01-13 17:12:56 +0200

    update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    saveBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', clickToStart);
+refs.saveBtn.addEventListener('click', clickToStop);
+
+refs.saveBtn.setAttribute("disabled", "disabled");
+
+function clickToStart() {
+    timerId = setInterval(() => {
+       setAttributeToStart()
+       refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function clickToStop() {
+    setAttributeToStop()
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setAttributeToStart() {
+     refs.startBtn.setAttribute("disabled", "disabled");
+     refs.saveBtn.removeAttribute("disabled");
+}
+
+function setAttributeToStop() {
+    refs.startBtn.removeAttribute("disabled");
+    refs.saveBtn.setAttribute("disabled", "disabled");
+}

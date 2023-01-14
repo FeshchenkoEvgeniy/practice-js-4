5f1f81e63d99733e738c529ee8ee3af47c4ba4a2
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-01-14 21:49:55 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onClick);
+
+
+function onClick(evt) {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = evt.currentTarget;
+  // createPromise(step.value, delay.value)
+  // console.log(delay.value, step.value, amount.value);
 }
+
+function createPromise(position, delay) {
+  return new Promise((resoleve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+  // console.log(shouldResolve)
+  if (shouldResolve) {
+    resoleve("true")
+  } else {
+    reject("false")
+  }
+   });
+}
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(position)
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });

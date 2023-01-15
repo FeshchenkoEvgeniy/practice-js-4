1eb00679269605099c6ed8f2263342ccfa963ed2
FeshchenkoEvgeniy <92670789+FeshchenkoEvgeniy@users.noreply.github.com>
2023-01-15 20:01:12 +0000

    Deploying to gh-pages from @ FeshchenkoEvgeniy/goit-js-hw-09@ee9b82a6c056f2a49d59c92e676de737bac984eb 🚀

---
+++ 03-promises.9b0540c6.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();const{elements:{delay:o,step:t,amount:l}}=e.currentTarget;console.log(o.value),console.log(t.value),console.log(l.value)}));
+//# sourceMappingURL=03-promises.9b0540c6.js.map
